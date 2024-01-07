ae670110b7e8fac14002fc1e9ad2df76efb4bc49
Braulio Santos <brauliosantos12@outlook.com>
2024-01-07 18:58:32 +0000

    new sets

--- fract-ol.c
+++ fract-ol.c
@@ -2,59 +2,142 @@
 
 int close_window(void)
 {
-    exit(0);
-}
-void close_exit(t_fractal fract)
-{
-    mlx_hook(fract.win, 17, 0L, close_window, &fract);
-    mlx_hook(fract.win, 2, 1L<<0, close_window, &fract);
-}
-void ft_mandelbrot(t_fractal fract)
-{
-    fract.mlx = mlx_init(); // inicia a minilibx library
-    fract.win = mlx_new_window(fract.mlx, 640, 418, "Fract-ol - 42"); // cria a nova janela com comprimento e tamanho e titulo necessario
-    close_exit(fract);
-    mlx_loop(fract.mlx);
-}
-void ft_julia(t_fractal fract)
-{
-    fract.mlx = mlx_init(); // inicia a minilibx library
-    fract.win = mlx_new_window(fract.mlx, 640, 418, "Fract-ol - 42"); // cria a nova janela com comprimento e tamanho e titulo necessario
-    close_exit(fract);
-    mlx_loop(fract.mlx);
-}
-void draw_fractal(t_fractal fract)
-{
-    fract.x = 0;
-    fract.y = 0;
-    while (fract.x < SIZE)
-    {
-        while (fract.y < SIZE)
-        {
-             if (ft_strncmp(fract.name, "Mandelbrot", 10) == 0)
-                ft_mandelbrot(fract);
-            else if (ft_strncmp(fract.name, "Julia", 6) == 0)
-                 ft_julia(fract);
-            else
-            {
-                ft_printf("Avaliable fractals: Mandelbroot or Julia\n");
-                exit(0);
-            }
-            fract.y++;
-        }
-        fract.x++;
-        fract.y = 0;
-    }
+	exit(0);
 }
 
+void close_exit(t_fractal *fract)
+{
+	mlx_hook(fract->window, 17, 0L, close_window, fract);
+	mlx_key_hook(fract->window, close_window, fract);
+}
 
+void put_color_to_pixel(t_fractal *fract, int x, int y, int color)
+{
+	char *temp;
+
+	temp = fract->pointer_to_image + (y * fract->line_length + x * (fract->bits_per_pixel / 8));
+	*(unsigned int *)temp = color;
+}
+
+void ft_mandelbrot(t_fractal *fract)
+{
+	int i;
+	double x_temp;
+
+	i = 0;
+	fract->zreal = 0;
+	fract->zimaginary = 0;
+	fract->creal = (fract->x / fract->zoom) + fract->offset_x;
+	fract->cimaginary = (fract->y / fract->zoom) + fract->offset_y;
+	while (++i < MAX_ITERATIONS)
+	{
+		x_temp = fract->zreal * fract->zreal - fract->zimaginary * fract->zimaginary + fract->creal;
+		fract->zimaginary = 2.0 * fract->zreal * fract->zimaginary + fract->cimaginary;
+		fract->zreal = x_temp;
+		if (fract->zreal * fract->zreal + fract->zimaginary * fract->zimaginary >= __DBL_MAX__)
+			break; // Escape if it diverges
+	}
+	if (i == fract->max_iterations)
+		put_color_to_pixel(fract, fract->x, fract->y, 0x000000);
+	else
+		put_color_to_pixel(fract, fract->x, fract->y, (fract->color * i));
+}
+
+void ft_julia(t_fractal *fract)
+{
+ int  i;
+ double tmp;
+
+ fract->name = "julia";
+ fract->zreal = fract->x / fract->zoom + fract->offset_x;
+ fract->zimaginary = fract->y / fract->zoom + fract->offset_y;
+ i = 0;
+ while (++i < fract->max_iterations)
+ {
+  tmp = fract->zreal;
+  fract->zreal = fract->zreal * fract->zreal - fract->zimaginary * fract->zimaginary
+   + fract->creal;
+  fract->zimaginary = 2 * fract->zimaginary * tmp + fract->cimaginary;
+  if (fract->zreal * fract->zreal + fract->zimaginary
+   * fract->zimaginary >= __DBL_MAX__)
+   break ;
+ }
+ if (i == fract->max_iterations)
+  put_color_to_pixel(fract, fract->x, fract->y, 0x000000);
+ else
+  put_color_to_pixel(fract, fract->x, fract->y, (fract->color * (i
+     % 255)));
+}
+void draw_fractal(t_fractal *fract, char *set)
+{
+	fract->x = 0;
+	fract->y = 0;
+	while (fract->x < SIZE)
+	{
+		while (fract->y < SIZE)
+		{
+			if (ft_strncmp(set, "mandelbrot", 10) == 0)
+				ft_mandelbrot(fract);
+			else if (ft_strncmp(set, "julia", 5) == 0)
+			{
+				if (!fract->creal && !fract->cimaginary)
+				{
+					fract->creal = -0.745429;
+					fract->cimaginary = 0.05;
+				}
+				ft_julia(fract);
+			}
+			else
+			{
+				ft_printf("Avaliable fractals: mandelbrot or julia\n");
+				exit(0);
+			}
+			fract->y++;
+		}
+		fract->x++;
+		fract->y = 0;
+	}
+	mlx_put_image_to_window(fract->mlx, fract->window, fract->image, 0, 0);
+}
+void init_mlx(t_fractal *fract)
+{
+	fract->mlx = mlx_init(); // inicia a minilibx library
+	fract->window = mlx_new_window(fract->mlx, SIZE, SIZE, fract->name);
+	if (fract->window == NULL)
+	{
+		free(fract->window);
+		free(fract->mlx);
+		exit(EXIT_FAILURE);
+	}
+	fract->image = mlx_new_image(fract->mlx, SIZE, SIZE);
+	fract->pointer_to_image = mlx_get_data_addr(fract->image, &fract->bits_per_pixel, &fract->line_length, &fract->endian);
+}
+
+void init_fract(t_fractal *fract)
+{
+	fract->x = 0;
+	fract->y = 0;
+	fract->color = 0xFCBE11;
+	fract->zoom = 260;
+	fract->offset_x = -2.10;
+	fract->offset_y = -1.50;
+	fract->max_iterations = 42;
+}
 int main(int ac, char **av)
 {
-    t_fractal fract;
-    fract.name = av[1];
-    if (ac != 2)
-        return(ft_printf("Please choose a set: Mandelbroot or Julia\n"), 0);
-    else
-        draw_fractal(fract);
-    return (0);
+	t_fractal *fract;
+
+	fract = malloc(sizeof(t_fractal));
+	if (ac != 2)
+		return (ft_printf("Please choose a set: mandelbrot or julia\n"), 0);
+	if (ac == 2)
+	{
+		fract->name = av[1];
+		init_mlx(fract);
+		init_fract(fract);
+		draw_fractal(fract, av[1]);
+		close_exit(fract);
+		mlx_loop(fract->mlx);
+	}
+	return (0);
 }
--- fractol.h
+++ fractol.h
@@ -2,16 +2,20 @@
 # define FRACTOL_H
 
 # include <unistd.h>
-# include <stdio.h> 
-# include <string.h>
 # include <stdlib.h>
-# include <limits.h>
 # include <stdbool.h>
 # include "minilibx-linux/mlx.h"
 # include "minilibx-linux/mlx_int.h"
 # include "libft.h"
 # include "printf.h"
-# include <stdarg.h>
+# include <limits.h>
+# include <float.h>
+# include <X11/X.h>
+# include <X11/keysym.h>
+
+# define HEIGHT 1000
+# define WIDTH 1000
+#define MAX_ITERATIONS 1000
 # define SIZE 700
 # define ESC 53
 # define UP 126
@@ -26,15 +30,45 @@
 # define M 46
 # define SCROLL_UP 4
 # define SCROLL_DOWN 5
+# define KEY_PLUS 69  // Adjust with the actual keycode for '+'
+# define KEY_MINUS 78 // Adjust with the actual keycode for '-'
+# define KEY_PAD_PLUS 65451 // Adjust with the actual keycode for numpad '+'
+# define KEY_PAD_MINUS 65453 // Adjust with the actual keycode for numpad '-'
 
 
-typedef struct  s_fractal
+// typedef struct	s_fractal
+// {
+// 	void	*mlx;
+// 	void	*win;
+// 	void	*image;
+// 	char	*name;
+// 	int		x;
+// 	int		y;
+// }		t_fractal;
+
+typedef struct s_fractal
 {
-    void    *mlx;
-    void    *win;
-    void    *image;
-    int     x;
-    int     y;
-    char    *name;
-}               t_fractal;
+	void	*mlx;
+	void	*window;
+	void	*image;
+	char		*pointer_to_image;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+	int		x;
+	int		y;
+	double	zreal;
+	double	zimaginary;
+	double	creal;
+	double	cimaginary;
+	int		color;
+	double	offset_x;
+	double	offset_y;
+	double	zoom;
+	char	*name;
+	int		max_iterations;
+}			t_fractal;
+
+
+void draw_fractal(t_fractal *fract, char *set);
 #endif
